158330c4ab9547b4c990f27b1df25f012b9b80ae
Omskka <92455486+Omskka@users.noreply.github.com>
2023-05-07 16:35:52 +0300

    Add files via upload

---
+++ push_swap6/inc/push_swap.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okirca <okirca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/24 17:59:40 by okirca            #+#    #+#             */
+/*   Updated: 2023/05/07 15:23:35 by okirca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct stack{
+	int	*a;
+	int	*b;
+	int	size_a;
+	int	size_b;
+}	t_stack;
+
+int			ft_lessthanpivot(t_stack *stack, int pivot);
+void		ft_checker(const char *argv);
+void		ft_dup(int argc, char **argv);
+void		ft_alreadysorted(int argc, char **argv);
+void		ft_quicksort(t_stack *stack, int size, int *arr);
+int			*ft_pushswap(int argc, int *arr, int ctl);
+void			ft_stacksorted(t_stack *stack);
+int			ft_median(int *arr, int size);
+void		ft_error(void);
+void		sa(t_stack	*stack);
+void		sb(t_stack *stack);
+void		ss(t_stack *stack);
+void		pa(t_stack *stack);
+void		pb(t_stack *stack);
+void		ra(t_stack *stack);
+void		rb(t_stack *stack);
+void		rr(t_stack *stack);
+void		rra(t_stack *stack);
+void		rrb(t_stack *stack);
+void		rrr(t_stack *stack);
+
+#endif
